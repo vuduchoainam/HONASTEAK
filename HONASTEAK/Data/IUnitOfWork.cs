6766225888c4a5792302944d25b3c57d20dadbdf
vuduchoainam <vinhoainam0815@gmail.com>
2023-05-24 00:11:06 +0700

    create unitofwork

---
+++ HONASTEAK/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HONASTEAK.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
