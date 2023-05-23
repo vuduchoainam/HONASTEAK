813d7e33f385a249d0ad8bca2cd2e4df2d0431c6
vuduchoainam <vinhoainam0815@gmail.com>
2023-05-24 00:10:53 +0700

    create repository

---
+++ HONASTEAK/Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HONASTEAK.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
