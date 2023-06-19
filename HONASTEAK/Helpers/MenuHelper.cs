8a2ca3ac95bda0956ef58f6a7cb2633faa618940
phuchau.tea <phuchau.tea@gmail.com>
2023-06-19 13:41:49 +0700

    feat: menu by category

---
+++ HONASTEAK/Helpers/MenuHelper.cs
@@ -0,0 +1,34 @@
+﻿using HONASTEAK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HONASTEAK.Helpers
+{
+    public static class MenuHelper
+    {
+        public static MvcHtmlString Menu(this HtmlHelper helper)
+        {
+            string hostUrl = HttpContext.Current.Request.Url.Authority;
+            string host = "https://" + hostUrl + "";
+
+            StringBuilder result = new StringBuilder();
+            using (var context = new ApplicationDbContext())
+            {
+                var categories = context.Categories.ToList();
+
+                foreach (var category in categories)
+                {
+                    result.Append("<li class='third-level'></li>");
+                    result.AppendFormat("<a href='{0}/Collections/{1}'>{2}</a>", host, category.Slug, category.Name);
+                    result.Append("</li>");
+                }
+            }
+
+            return new MvcHtmlString(result.ToString());
+        }
+    }
+}
